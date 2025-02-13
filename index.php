67058baa9bb4e797a04f8273100d7b6cf4cdd422
Chiby25 <gracenmeso25@gmail.com>
2025-02-13 16:14:38 +0100

    firstcommit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- <?php echo "hello baby girl";?> -->
+
+<?php
+ $number = 21;
+
+ if ($number % 2 == 0) {
+    echo "$number is even number."; }
+
+ else 
+ {echo "$number is an odd number.";}
+ ?>
