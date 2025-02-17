63f36f99f8232b0a008c1f6e2e3856612610427f
Chiby25 <gracenmeso25@gmail.com>
2025-02-17 14:22:12 +0100

    kalmeNee

---
+++ ex.php
@@ -0,0 +1,16 @@
+<!-- <?php echo "hello baby girl";?> -->
+
+
+
+<!-- php code to show even and odd numbers -->
+
+<?php
+ $number = $_POST["name"];
+
+ if ($number % 2 == 0) {
+    echo "$number is even number."; }
+
+ else 
+ {echo "$number is an odd number.";}
+ ?> 
+
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<!-- <?php echo "hello baby girl";?> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+   
+<form method="POST" action="ex.php">
+<input type="number" name="name" placeholder="Enter number">
+<button type="submit">Submit</button>
 
-<?php
- $number = 21;
-
- if ($number % 2 == 0) {
-    echo "$number is even number."; }
-
- else 
- {echo "$number is an odd number.";}
- ?>
+  </body>
+  </html>
